a114589035eb7b36a8beca4b34f4b25c45be385c
aphengsene <mrantegra@gmail.com>
2016-10-27 08:21:20 -0500

    update to lab1.

--- lab1.rb
+++ lab1.rb
@@ -1,5 +1,5 @@
 ##############################################################################
-# 
+#
 # Update this script to:
 #
 # - Ask for the person's name
@@ -11,3 +11,19 @@
 # - Thank the person by name for stopping by
 #
 #############################################################################
+
+puts 'Hello there, and what\'s your name?'
+name = gets
+puts 'Your name is ' + name +'what a awesome name!'
+
+puts 'How old are you?'
+age = gets
+var = 365 * 24 * 60
+puts + name + ', did you know you are' var * + age + 'minutes old?'
+
+puts 'what is the temperature today'
+temperature = gets
+(+ temperature + - 32) / 1.8
+puts + name +', the temperature is' + temperature + 'degrees Celsius.'
+
+puts 'thank you,' + name + '!'
